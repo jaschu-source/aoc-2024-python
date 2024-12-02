9fa8319828167d8002df4225a13efb6135f0774f
jaschu-resource <jas.schulze@gmail.com>
2024-12-02 07:04:36 +0100

    D01-1 & D01-2 solutions

--- day01/day01.py
+++ day01/day01.py
@@ -3,13 +3,40 @@ import os
 from aocd import get_data
 
 def parse(puzzle_input):
-    """Parse input."""
+    puzzle = []
+    puzzle_left = []
+    puzzle_right = []
+
+    for line in puzzle_input.split("\n"):
+        split_line = line.split()
+        puzzle_left.append(split_line[0])
+        puzzle_right.append(split_line[1])
+    
+    puzzle.append(puzzle_left)
+    puzzle.append(puzzle_right)
+
+    return puzzle
 
 def part1(data):
-    """Solve part 1."""
+    result = 0
+    data[0].sort()
+    data[1].sort()
+    for index, value in enumerate(data[0]):
+        if data[0][index] >= data[1][index]:
+            result = result + (int(data[0][index]) - int(data[1][index]))
+        else:
+            result = result + (int(data[1][index]) - int(data[0][index]))
+    return result
 
 def part2(data):
-    """Solve part 2."""
+    result = 0
+    for right_number in data[0]:
+        count = 0
+        for left_number in data[1]:
+            if right_number == left_number:
+                count = count + 1
+        result = result + int(right_number) * count
+    return result
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -2,7 +2,7 @@
 
 import pathlib
 import pytest
-import template.aoc_template as aoc
+import day01 as aoc
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
@@ -16,20 +16,17 @@ def example2():
     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [['3', '4', '2', '1', '3', '3'], ['4', '3', '5', '3', '9', '3']]
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 11
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
-    assert aoc.part2(example1) == ...
+    assert aoc.part2(example1) == 31
 
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example2(example2):
