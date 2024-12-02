bdbd32c41fdc3b4d32d27028a0687840acdaf19d
jaschu-resource <jas.schulze@gmail.com>
2024-12-02 07:09:30 +0100

    remove unneeded code

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -9,11 +9,6 @@ def example1():
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
-@pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
-    return aoc.parse(puzzle_input)
-
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1 == [['3', '4', '2', '1', '3', '3'], ['4', '3', '5', '3', '9', '3']]
@@ -25,8 +20,3 @@ def test_part1_example1(example1):
 def test_part2_example1(example1):
     """Test part 2 on example input."""
     assert aoc.part2(example1) == 31
-
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
-    """Test part 2 on example input."""
-    assert aoc.part2(example2) == ...
